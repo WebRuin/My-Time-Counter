2fc2be0ead3e2253a6a6daaf636bde4c27c3ee0b
Tim Smith <webruin@icloud.com>
2015-09-18 01:58:59 -0700

    Initial Commit

---
+++ My Time Counter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  My Time Counter
+//
+//  Created by Tim Smith on 9/18/15.
+//  Copyright © 2015 WebRuin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
